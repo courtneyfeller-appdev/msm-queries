33f5dda556d7974a7b5ebf31a47ae6f8fb9c7352
courtneyfeller <courtneyfeller@uchicago.edu>
2020-11-07 09:42:44 +0000

    first commit

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,22 @@
+class DirectorsController < ApplicationController
+
+  def index
+    @list_of_directors = Director.all
+    render({ :template => "director_templates/index.html.erb"})
+  end
+
+  def eldest
+    @oldest = Director.where.not({ :dob => nil }).order({ :dob => :asc }).at(0)
+    render({ :template => "director_templates/eldest.html.erb"})
+  end
+
+  def youngest
+    @youngest = Director.where.not({ :dob => nil }).order({ :dob => :desc }).at(0)
+    render({ :template => "director_templates/youngest.html.erb"})
+  end
+
+  def director_details
+    render({ :template => "director_templates/show.html.erb" })
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/eldest", { :controller => "directors", :action => "eldest" })
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+  get("/directors/:director_id", { :controller => "directors", :action => "director_details" })
+
+  get("/movies", { :controller => "movies", :action => "index" })
+
+  get("/actors", { :controller => "actors", :action => "index" })
 end
